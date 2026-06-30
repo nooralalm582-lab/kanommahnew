fca9689f61eaa964459087fa4e0c59984af17063
nooralalm582-lab <nooralalm582@gmail.com>
2026-06-30 11:22:52 +0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,48 +1,24 @@
 import { auth } from "./firebase.js";
-import {
-  createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 const form = document.getElementById("signupForm");
 
-form.onsubmit = async function (e) {
-
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    alert("تم الضغط على الزر");
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
 
-    const email = document.getElementById("signupEmail").value.trim();
-    const password = document.getElementById("signupPassword").value;
-    const confirm = document.getElementById("confirmPassword").value;
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
 
-    if(password !== confirm){
-        alert("كلمتا المرور غير متطابقتين");
-        return;
+        alert("تم إنشاء الحساب بنجاح");
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+        console.log(error);
+
+        alert(error.message);
     }
-
-    try{
-
-        const user = await createUserWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-
-        alert("تم إنشاء الحساب");
-
-        console.log(user);
-
-        window.location.href="login.html";
-
-    }catch(error){
-
-        console.error(error);
-
-        alert(error.code + "\n" + error.message);
-
-    }
-
-}
-    }
-
 });
